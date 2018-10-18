243449a6d0602ec018c5adc5c88fa860c5f6c1de
fedecapi <feddy.capi@gmail.com>
2018-10-18 11:57:20 +0200

    Messaggio per la commit

---
+++ src/Dotted.js
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import './Dotted.css';
+
+const DottedBox = () => (
+  <div className="DottedBox">
+    <p className="DottedBox_content">Get started with CSS styling</p>
+  </div>
+);
+
+export default DottedBox;
